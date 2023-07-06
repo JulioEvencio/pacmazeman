e50b47e182c0647fc85697c56d8855ee111bc6f9
julio <julioigreja.dev@gmail.com>
2023-07-06 20:06:28 -0300

    update: update credits

--- pacmazeman/src/game/screens/Credits.java
+++ pacmazeman/src/game/screens/Credits.java
@@ -10,6 +10,15 @@ public class Credits extends Screen {
 	protected void renderString(Graphics graphics) {
 		graphics.drawString("> Programmer: Júlio Igreja", Game.WIDTH * Game.SCALE / 2 - 300, 150);
 		graphics.drawString("> Access: https://github.com/JulioEvencio", Game.WIDTH * Game.SCALE / 2 - 300, 180);
+		
+		graphics.drawString("> Sound - menu select: InspectorJ", Game.WIDTH * Game.SCALE / 2 - 300, 210);
+		graphics.drawString("> Access: https://freesound.org/people/InspectorJ", Game.WIDTH * Game.SCALE / 2 - 300, 240);
+		
+		graphics.drawString("> Sound - menu change: victorium183", Game.WIDTH * Game.SCALE / 2 - 300, 270);
+		graphics.drawString("> Access: https://freesound.org/people/victorium183", Game.WIDTH * Game.SCALE / 2 - 300, 300);
+		
+		graphics.drawString("> Sound - menu loop: wi-photos", Game.WIDTH * Game.SCALE / 2 - 300, 330);
+		graphics.drawString("> Access: https://freesound.org/people/wi-photos", Game.WIDTH * Game.SCALE / 2 - 300, 360);
 	}
 
 }
