fb944bc181688cf9ef7ab252cff31f7e62aa5199
JulioEvencio <julioigreja.dev@gmail.com>
2023-07-17 21:24:54 -0300

    update: update tutorial

--- pacmazeman/src/game/screens/Tutorial.java
+++ pacmazeman/src/game/screens/Tutorial.java
@@ -9,8 +9,9 @@ public class Tutorial extends Screen {
 	@Override
 	protected void renderString(Graphics graphics) {
 		graphics.drawString("> W, A, S, D -> use to move", Game.WIDTH * Game.SCALE / 2 - 300, 150);
-		graphics.drawString("> F3 -> to enable/disable FPS", Game.WIDTH * Game.SCALE / 2 - 300, 180);
-		graphics.drawString("> F4 -> to enable/disable musics and sounds", Game.WIDTH * Game.SCALE / 2 - 300, 210);
+		graphics.drawString("> ESC or P -> use to pause the game", Game.WIDTH * Game.SCALE / 2 - 300, 180);
+		graphics.drawString("> F3 -> to enable/disable FPS", Game.WIDTH * Game.SCALE / 2 - 300, 210);
+		graphics.drawString("> F4 -> to enable/disable musics and sounds", Game.WIDTH * Game.SCALE / 2 - 300, 240);
 	}
 
 }
